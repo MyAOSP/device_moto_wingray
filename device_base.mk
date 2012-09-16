2580f77934e0ce25a1f7b6a3fa0832c5b1206762
travp624 <travp624@gmail.com>
2012-09-16 10:05:27 -0500

    fix wingray back to ours

--- device_base.mk
+++ device_base.mk
@@ -115,10 +115,10 @@ PRODUCT_COPY_FILES += \
     system/extras/bugmailer/send_bug:system/bin/send_bug
 
 # inherit from the non-open-source side, if present
-ifneq ($(filter trygon trygon_l10n calgon full_stingray tyranid stingray cm_stingray,$(TARGET_PRODUCT)),)
-$(call inherit-product-if-exists, vendor/motorola/stingray/stingray-vendor.mk)
+ifneq ($(filter trygon trygon_l10n calgon full_stingray tyranid stingray baked_stingray,$(TARGET_PRODUCT)),)
+$(call inherit-product-if-exists, vendor/moto/stingray/stingray-vendor.mk)
 else
-$(call inherit-product-if-exists, vendor/motorola/stingray/wingray-vendor.mk)
+$(call inherit-product-if-exists, vendor/moto/stingray/wingray-vendor.mk)
 endif
 
 WIFI_BAND := 802_11_ABG
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := vendor/moto/wingray
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
 
-# Motorola blobnecessary for wingray hardware
-PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/proprietary/camera.stingray.so:system/lib/hw/camera.stingray.so
+# add_lunch_combo full_wingray-userdebug
